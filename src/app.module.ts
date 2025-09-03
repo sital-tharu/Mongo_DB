0f07115915ec6c60e1e2c2a9001309528d9ee816
sital-tharu <sitatharu769@gmail.com>
2025-09-03 23:13:54 +0530

    connecting mongo in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { EnvService } from './env/env.service';
 import { EnvController } from './env/env.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-  })],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONOGO_URI!),
+
+],
   controllers: [AppController, EnvController],
   providers: [AppService, EnvService],
 })
