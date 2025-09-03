8b7e541d73e3c5bafeb3c8385fd44e084c011966
sital-tharu <sitatharu769@gmail.com>
2025-09-03 22:53:13 +0530

    fixing the controller error

--- src/env/env.controller.ts
+++ src/env/env.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { EnvService } from './env.service';
 
 @Controller('env')
-export class EnvController {}
+export class EnvController {
+    constructor(private evService: EnvService){
+
+    }
+    @Get()
+    getUrl(){
+    return this.evService.getDbUrl();
+}
+}
