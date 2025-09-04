eba97237bae2fb59398458f8d9dad5056cc8c3db
sital-tharu <sitatharu769@gmail.com>
2025-09-04 17:46:51 +0530

    giving structure and name

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Student, StudentSchema } from './student.schema';
 
-@Module({})
+@Module({
+    imports: [
+        MongooseModule.forFeature([{name: Student.name, schema: StudentSchema}])
+    ]
+})
 export class StudentModule {}
