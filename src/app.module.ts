75eac122128dedba974f06299cba5f3d0a23834f
sital-tharu <sitatharu769@gmail.com>
2025-09-04 17:29:11 +0530

    creating module and creating schema for strudent

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { ConfigModule } from '@nestjs/config';
 import { EnvService } from './env/env.service';
 import { EnvController } from './env/env.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { StudentModule } from './student/student.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONOGO_URI!),
+    StudentModule,
 
 ],
   controllers: [AppController, EnvController],
