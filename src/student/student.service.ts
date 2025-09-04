bd30bf2bcd0b8c2f99bd0beb8f6838fafc87996d
sital-tharu <sitatharu769@gmail.com>
2025-09-04 22:44:00 +0530

    feat: add StudentService with createStudent method for MongoDB integration

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Student, StudentDocument } from './student.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    constructor(
+        @InjectModel(Student.name) private studentModel: Model<StudentDocument>
+    )
+    {}
+
+    async createStudent(data: Partial<Student>):
+     Promise<Student> {
+        const newStudent = new this.studentModel(data);
+        return newStudent.save();
+     }
+}
