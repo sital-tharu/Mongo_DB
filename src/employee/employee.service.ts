80c881de652558071770c2976b06407e8c996cf4
sital-tharu <sitatharu769@gmail.com>
2025-09-10 00:10:24 +0530

    When create employ meathod is called profile and employee data gets insert

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Employee } from './schema/employee.schema';
+import { Model } from 'mongoose';
+import { Profile } from './schema/profile.schema';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+    constructor(
+        @InjectModel(Employee.name) private employeeModel : Model<Employee>,
+        @InjectModel(Profile.name) private profileModel : Model<Profile>
+    ){}
+
+    async createEmployee() : Promise<Employee> {
+        const profile = await new this.profileModel({
+            age : 20, 
+            qualification : 'BCA'
+        }).save();
+
+        const employee = new this.employeeModel({
+            name : 'anu',
+            profile : profile._id
+    });
+    return employee.save();
+
+}
+}
