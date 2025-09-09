23ddb02d6481adcd3f5e778677d4ec996f5db0d5
sital-tharu <sitatharu769@gmail.com>
2025-09-10 00:18:39 +0530

    Importing post and get request

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { EmployeeService } from './employee.service';
 
 @Controller('employee')
-export class EmployeeController {}
+export class EmployeeController {
+    constructor (private readonly employeeService: EmployeeService){}
+
+    @Post()
+    create(){
+        return this.employeeService.createEmployee();
+    }
+    @Get()
+    gitAll(){
+        return this.employeeService.findAll();
+    }
+}
