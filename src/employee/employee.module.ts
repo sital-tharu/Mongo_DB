deaeb7a541917f4e36d8d2efad7a4b5ed42de92d
sital-tharu <sitatharu769@gmail.com>
2025-09-10 00:01:00 +0530

    importing MongooseModule in the employee.modules

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EmployeeSchema } from './schema/employee.schema';
+import { ProfileSchema } from './schema/profile.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Employee', schema: EmployeeSchema },
+      { name: 'Profile', schema: ProfileSchema }
+    ]),
+  ],
   providers: [EmployeeService],
   controllers: [EmployeeController]
 })
