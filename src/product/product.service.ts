a522d2b756b02c4491ac91a0dee677236fc3ca05
sital-tharu <sitatharu769@gmail.com>
2025-09-10 23:14:03 +0530

    save in mongo db and save

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product } from './schemas/product.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+
+    constructor(@InjectModel(Product.name) 
+    private productModel: Model<Product>){}
+
+    async createProduct() :Promise<Product>{
+        const product = new this.productModel
+        ({
+            title: 'Gamming Pc',
+            tags : [
+                {name: 'electronic'},
+                {name: 'gaming'},
+                {name: 'pc'}
+            ]
+
+        })
+        return product.save();
+    }
+}
