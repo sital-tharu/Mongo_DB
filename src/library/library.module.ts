2ee637a4fcc362a9509793cee5764a650376297d
sital-tharu <sitatharu769@gmail.com>
2025-09-11 22:33:53 +0530

    importing MongooseModule.forFeature

--- src/library/library.module.ts
+++ src/library/library.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { LibraryService } from './library.service';
 import { LibraryController } from './library.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { libary, libarySchema } from './schemas/libary.schema';
+import { Book, schemaFactory } from './schemas/book.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+     { name: libary.name, schema: libarySchema},
+      {name: Book.name, schema: schemaFactory},
+    ])
+  ],
   providers: [LibraryService],
   controllers: [LibraryController]
 })
