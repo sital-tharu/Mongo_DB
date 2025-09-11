48dab88f2ead119bb297cf02cf36d80974b0d673
sital-tharu <sitatharu769@gmail.com>
2025-09-11 23:01:52 +0530

    http post request and get request

--- src/library/library.controller.ts
+++ src/library/library.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { LibraryService } from './library.service';
 
 @Controller('library')
-export class LibraryController {}
+export class LibraryController {
+    constructor(
+        private readonly libaryService : LibraryService
+    ){}
+
+    @Post()
+    createLibrary(){
+        return this.libaryService.createLibrary();
+    }
+
+    @Get()
+    getLibaries(){
+        return this.libaryService.getLibraries();
+    }
+}
