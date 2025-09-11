d8ac1281871574896b54de876cb718de1f0aec1d
sital-tharu <sitatharu769@gmail.com>
2025-09-11 22:36:49 +0530

    Injecting our models

--- src/library/library.service.ts
+++ src/library/library.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Book } from './schemas/book.schema';
+import { Model } from 'mongoose';
+import { libary } from './schemas/libary.schema';
 
 @Injectable()
-export class LibraryService {}
+export class LibraryService {
+    constructor(
+        @InjectModel(Book.name) private bookModel: Model<Book>,
+        @InjectModel(libary.name) private libaryModel : Model<libary>
+    ){}
+}
