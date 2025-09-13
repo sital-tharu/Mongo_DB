90ba203a1cc79dd7cfeb6bab071af4fc16dcb953
sital-tharu <sitatharu769@gmail.com>
2025-09-13 15:50:56 +0530

    Injecting Models

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Developer } from './schema/developers.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Project } from './schema/project.schema';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+    constructor (
+        @InjectModel(Developer.name) private developerModel: Model<Developer>,
+        @InjectModel(Project.name) private projectModel: Model<Project>,
+    ) {}
+}
