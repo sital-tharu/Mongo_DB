72bbce55fb757ff9652ce2d1306e0e504cca87fb
sital-tharu <sitatharu769@gmail.com>
2025-09-21 23:16:15 +0530

    Feat: define User schema with email and password fields

--- src/auth/user.schema.ts
+++ src/auth/user.schema.ts
@@ -2,6 +2,12 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import {Document} from "mongoose";
 
 export type UserDocument = User & Document;
+@Schema()
+export class User {
+    @Prop({required: true, unique: true})
+    email: String;
 
-
-export class User {}
+    @Prop({required: true})
+    password: String;
+}
+export const UserSchema = SchemaFactory.createForClass(User);
