20a16936470b029f42f376e6ba3ff2cf85b9f479
sital-tharu <sitatharu769@gmail.com>
2025-09-22 23:40:24 +0530

    feat(auth): scaffold AuthService with user model and JWT service injection

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './user.schema';
+import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(@InjectModel(User.name) private userModel :
+     Model<UserDocument>, private jwtService: JwtService ) {
+        
+    }
+
+    
+}
